d5e691395983c1762a0260364b65ac34c45fa8a1
Jimichelle <jimi.denysiak@icloud.com>
2024-12-02 22:33:45 +0100

    Update user redirection in inscription.php; redirect to login page

--- pages/auth/inscription.php
+++ pages/auth/inscription.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['user'] = $email;
 
             // Rediriger vers la page d'accueil
-            header('Location: /');
+            header('Location: /connexion');
             exit;
         }
     }
